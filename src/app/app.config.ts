03a66a0771ba3de4f3fd0316f928a15445f4152a
Rida jahan <140480103+rida2911@users.noreply.github.com>
2025-07-06 01:36:35 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,17 +9,7 @@ import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from
 import { getPerformance, providePerformance } from '@angular/fire/performance';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
-// export const appConfig: ApplicationConfig = {
-//   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "portfolio-khyati-satija", appId: "1:267463640847:web:99454747893731d16aa07a", storageBucket: "portfolio-khyati-satija.firebasestorage.app", apiKey: "AIzaSyAcav-xBGRhtzEx6aUqRRMSWTO8-oV-dmc", authDomain: "portfolio-khyati-satija.firebaseapp.com", messagingSenderId: "267463640847", measurementId: "G-L4QTJS145F" })), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideAppCheck(() => {
-//   // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
-//   // const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
-//   // return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
-// }), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideFirebaseApp(() => initializeApp({ projectId: "portfolio-khyati-satija", appId: "1:267463640847:web:99454747893731d16aa07a", storageBucket: "portfolio-khyati-satija.firebasestorage.app", apiKey: "AIzaSyAcav-xBGRhtzEx6aUqRRMSWTO8-oV-dmc", authDomain: "portfolio-khyati-satija.firebaseapp.com", messagingSenderId: "267463640847", measurementId: "G-L4QTJS145F" })), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideAppCheck(() => {
-//   // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
-//   // const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
-//   // return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
-// }), providePerformance(() => getPerformance()), provideStorage(() => getStorage())]
-// };
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
